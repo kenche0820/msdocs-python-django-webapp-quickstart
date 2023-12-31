4e2e9929e33522ae2c6a8d0b32889cc1097200de
Kenneth Cheung <kenneth.cheung@distributel.ca>
2023-12-31 18:11:38 -0500

    views.py speller

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -3,6 +3,20 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
 
+def speller():
+    # [START speller]
+    from azure.core.credentials import AzureKeyCredential
+    from azure.search.documents import SearchClient
+
+    service_endpoint = "https://ken-cog-search-svc.search.windows.net"
+    index_name = "sharepoint-index"
+    key = "quRm9N9F4y4vGhgZBFvNtRs86VqLugIBVVeyC0drYaAzSeDm86cn"
+
+    credential = AzureKeyCredential(key)
+    client = SearchClient(endpoint=service_endpoint, index_name=index_name, credential=credential)
+    results = list(client.search(search_text="How to add DIDs to a SIP Trunk?"))
+
+
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
