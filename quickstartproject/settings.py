47b458ab2d2836b04a1a037bc521493c885bb089
Kenneth Cheung <kenneth.cheung@distributel.ca>
2023-12-31 22:18:29 -0500

    settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hello_azure'
+    'hello_azure',
+    'azure.core.credentials',
+    'azure.search.documents'
 ]
 
 MIDDLEWARE = [                                                                   
