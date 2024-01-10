d3789989c050099fd33feca948e7469642ea8b7b
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-10 06:19:27 -0500

    views.py results

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -61,16 +61,13 @@ def hello(request):
         
         logging.error(f"fileNames: {fileNames}\n")
 
-        
-
-
         # [END semantic_ranking]        
         if name is None or name == '':
             print("Request for hello page received with no name or blank name -- redirecting")
             return redirect('index')
         else:
             print("Request for hello page received with name=%s" % caption.text)            
-            context = {'name': caption.text, 'file': fileNames }
+            context = {'name': caption.text, 'file': fileNames, 'results': results }
             return render(request, 'hello_azure/hello.html', context)
     else:
         return redirect('index')
