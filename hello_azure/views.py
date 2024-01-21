741282a59477ffb2ec7bc96d97196ed4f01cf4f6
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-20 21:15:34 -0500

    views.py torch

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,12 +1,5 @@
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
-
-from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
+import torch                
+from transformers import AutoTokenizer, AutoModelWithLMHead
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -69,27 +62,23 @@ def hello(request):
                 myCaption = caption.text
 
 
+        tokenizer = AutoTokenizer.from_pretrained('t5-base')                        
+        model = AutoModelWithLMHead.from_pretrained('t5-base', return_dict=True)      
+        text = "Former American President Donald Trump has come up with an excuse for his threats not to help Europe in the event of war, reports Politico. During a meeting with European Commission President Ursula von der Leyen in 2020, Trump promised not to help Europe in the event of war."
+
+        inputs = tokenizer.encode("summarize: " + text,                  
+        return_tensors='pt',              
+        max_length=512,             
+        truncation=True)     
+
+        summary_ids = model.generate(inputs, max_length=150, min_length=80, length_penalty=5., num_beams=2)  
+
+        summary = tokenizer.decode(summary_ids[0])      
 
 
 
-        LANGUAGE = "english"
-        SENTENCES_COUNT = 10
-        
-        url = "https://en.wikipedia.org/wiki/Automatic_summarization"
-        parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-        # or for plain text files
-        # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
-        # parser = PlaintextParser.from_string("Check this out.", Tokenizer(LANGUAGE))
-        stemmer = Stemmer(LANGUAGE)
+        tempContent = summary
 
-        summarizer = Summarizer(stemmer)
-        summarizer.stop_words = get_stop_words(LANGUAGE)
-
-        tempContent = ""
-
-        for sentence in summarizer(parser.document, SENTENCES_COUNT):
-            #print(sentence)
-            tempContent += sentence
 
 
 
