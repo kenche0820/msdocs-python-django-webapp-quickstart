9208738e00440236c1dee714470feec63d33f5ec
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-22 09:26:30 -0500

    views.py revert

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,7 +1,3 @@
-from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
-from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
-from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -62,34 +58,11 @@ def hello(request):
                 print(f"Caption: {caption.text}\n")
                 myCaption = caption.text
 
-
-
         tempOutput = "" 
         i = 0        
         for result in results:
-            #tempContent = result["content"]
-            #tempContent = tempContent[0:1000]    
-
-            document = result["content"]
-            #And instantiate objects and call the method.
-            # Object of automatic summarization.
-            auto_abstractor = AutoAbstractor()
-            # Set tokenizer.
-            auto_abstractor.tokenizable_doc = SimpleTokenizer()
-            # Set delimiter for making a list of sentence.
-            auto_abstractor.delimiter_list = [".", "\n"]
-            # Object of abstracting and filtering document.
-            abstractable_doc = TopNRankAbstractor()
-            # Summarize document.
-            result_dict = auto_abstractor.summarize(document, abstractable_doc)
-
-            tempContent=""
-            for sentence in result_dict["summarize_result"]:
-                print(sentence)
-                tempContent += sentence
-
-
-
+            tempContent = result["content"]    
+            tempContent = tempContent[0:1000]        
             tempOutput = tempOutput + result["metadata_spo_item_name"] + ";;" + str(round(result["@search.reranker_score"],2)) + ";;" + tempContent + ",,"
                         
         myRows = tempOutput.split(",,")      
