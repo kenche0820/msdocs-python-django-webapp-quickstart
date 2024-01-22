bbf7d8d6958f16ebd932f64561ad09f96f568b37
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-21 22:50:03 -0500

    views.py summarize

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -63,30 +63,33 @@ def hello(request):
                 myCaption = caption.text
 
 
-        document = "Natural language generation (NLG) is the natural language processing task of generating natural language from a machine representation system such as a knowledge base or a logical form. Psycholinguists prefer the term language production when such formal representations are interpreted as models for mental representations."
-        #And instantiate objects and call the method.
-        # Object of automatic summarization.
-        auto_abstractor = AutoAbstractor()
-        # Set tokenizer.
-        auto_abstractor.tokenizable_doc = SimpleTokenizer()
-        # Set delimiter for making a list of sentence.
-        auto_abstractor.delimiter_list = [".", "\n"]
-        # Object of abstracting and filtering document.
-        abstractable_doc = TopNRankAbstractor()
-        # Summarize document.
-        result_dict = auto_abstractor.summarize(document, abstractable_doc)
-
-        tempContent=""
-        for sentence in result_dict["summarize_result"]:
-            print(sentence)
-            tempContent += sentence
-
 
         tempOutput = "" 
         i = 0        
         for result in results:
-            #tempContent = result["content"]    
-            #tempContent = tempContent[0:1000]        
+            #tempContent = result["content"]
+            #tempContent = tempContent[0:1000]    
+
+            document = result["content"]
+            #And instantiate objects and call the method.
+            # Object of automatic summarization.
+            auto_abstractor = AutoAbstractor()
+            # Set tokenizer.
+            auto_abstractor.tokenizable_doc = SimpleTokenizer()
+            # Set delimiter for making a list of sentence.
+            auto_abstractor.delimiter_list = [".", "\n"]
+            # Object of abstracting and filtering document.
+            abstractable_doc = TopNRankAbstractor()
+            # Summarize document.
+            result_dict = auto_abstractor.summarize(document, abstractable_doc)
+
+            tempContent=""
+            for sentence in result_dict["summarize_result"]:
+                print(sentence)
+                tempContent += sentence
+
+
+
             tempOutput = tempOutput + result["metadata_spo_item_name"] + ";;" + str(round(result["@search.reranker_score"],2)) + ";;" + tempContent + ",,"
                         
         myRows = tempOutput.split(",,")      
