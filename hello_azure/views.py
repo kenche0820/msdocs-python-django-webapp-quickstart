6d332e8973699565f175437d4b5c958bf5f03c6e
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-22 15:47:47 -0500

    views.py nltk

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,4 +1,6 @@
-
+import nltk               
+from nltk.corpus import stopwords                       
+from nltk.tokenize import word_tokenize, sent_tokenize 
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -61,10 +63,44 @@ def hello(request):
                 myCaption = caption.text
 
 
+        text = "This is a sentence.   This is another sentence."  
+        stopWords = set(stopwords.words("english"))              
+        words = word_tokenize(text)     
+        freqTable = dict()                 
+        for word in words:               
+            word = word.lower()                 
+            if word in stopWords:                 
+                continue                  
+            if word in freqTable:                       
+                freqTable[word] += 1            
+            else:          
+                freqTable[word] = 1     
+        sentences = sent_tokenize(text)                 
+        sentenceValue = dict()                     
+
+        for sentence in sentences:               
+            for word, freq in freqTable.items():              
+                if word in sentence.lower():           
+                    if sentence in sentenceValue:                                 
+                        sentenceValue[sentence] += freq                       
+                    else:                       
+                        sentenceValue[sentence] = freq                    
+
+        sumValues = 0                        
+        for sentence in sentenceValue:              
+            sumValues += sentenceValue[sentence] 
+
+        average = int(sumValues / len(sentenceValue))   
+
+        summary = ''      
+
+        for sentence in sentences:               
+            if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):                
+                summary += " " + sentence                  
+        print(summary)         
 
 
 
-                
 
         tempOutput = "" 
         i = 0        
