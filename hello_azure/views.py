ef34a76177c9de56762dbe419d17e54740eb5bcc
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-22 15:35:10 -0500

    views.py revert

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,12 +1,4 @@
-from __future__ import absolute_import
-from __future__ import division, print_function, unicode_literals
 
-from sumy.parsers.html import HtmlParser
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer as Summarizer
-from sumy.nlp.stemmers import Stemmer
-from sumy.utils import get_stop_words
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -72,32 +64,13 @@ def hello(request):
 
 
 
-        LANGUAGE = "english"
-        SENTENCES_COUNT = 10
-        
-        url = "https://en.wikipedia.org/wiki/Automatic_summarization"
-        parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
-        # or for plain text files
-        # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
-        # parser = PlaintextParser.from_string("Check this out.", Tokenizer(LANGUAGE))
-        stemmer = Stemmer(LANGUAGE)
-
-        summarizer = Summarizer(stemmer)
-        summarizer.stop_words = get_stop_words(LANGUAGE)
-
-        tempContent = ""
-
-        for sentence in summarizer(parser.document, SENTENCES_COUNT):
-            #print(sentence)
-            tempContent += sentence
-
-
+                
 
         tempOutput = "" 
         i = 0        
         for result in results:
-            #tempContent = result["content"]    
-            #tempContent = tempContent[0:1000]        
+            tempContent = result["content"]    
+            tempContent = tempContent[0:1000]        
             tempOutput = tempOutput + result["metadata_spo_item_name"] + ";;" + str(round(result["@search.reranker_score"],2)) + ";;" + tempContent + ",,"
                         
         myRows = tempOutput.split(",,")      
