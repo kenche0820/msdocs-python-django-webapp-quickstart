55feba71f19802986e0998a5fc93ff12d31a72d2
Kenneth Cheung <kenneth.cheung@distributel.ca>
2024-01-22 22:54:24 -0500

    views.py top7

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,4 +1,9 @@
-import nltk               
+import bs4 as bs
+import urllib.request
+import re
+import nltk
+import heapq
+               
 from nltk.corpus import stopwords                       
 from nltk.tokenize import word_tokenize, sent_tokenize 
 
@@ -72,41 +77,54 @@ def hello(request):
 
 
 
-            text = result["content"]
-            stopWords = set(stopwords.words("english"))              
-            words = word_tokenize(text)     
-            freqTable = dict()                 
-            for word in words:               
-                word = word.lower()                 
-                if word in stopWords:                 
-                    continue                  
-                if word in freqTable:                       
-                    freqTable[word] += 1            
-                else:          
-                    freqTable[word] = 1     
-            sentences = sent_tokenize(text)                 
-            sentenceValue = dict()                     
 
-            for sentence in sentences:               
-                for word, freq in freqTable.items():              
-                    if word in sentence.lower():           
-                        if sentence in sentenceValue:                                 
-                            sentenceValue[sentence] += freq                       
-                        else:                       
-                            sentenceValue[sentence] = freq                    
+            #scraped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Severe_acute_respiratory_syndrome_coronavirus_2')
+            #article = scraped_data.read()
+            article = result["content"]
 
-            sumValues = 0                        
-            for sentence in sentenceValue:              
-                sumValues += sentenceValue[sentence] 
+            parsed_article = bs.BeautifulSoup(article,'lxml')
 
-            average = int(sumValues / len(sentenceValue))   
+            paragraphs = parsed_article.find_all('p')
 
-            summary = ''      
+            article_text = ""
 
-            for sentence in sentences:               
-                if (sentence in sentenceValue) and (sentenceValue[sentence] > (2 * average)):                
-                    summary += " " + sentence                  
+            for p in paragraphs:
+                article_text += p.text
+
+            # Removing Square Brackets and Extra Spaces
+            article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)
+            article_text = re.sub(r'\s+', ' ', article_text)
+            # Removing special characters and digits
+            formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )
+            formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)
+            sentence_list = nltk.sent_tokenize(article_text)
+            stopwords = nltk.corpus.stopwords.words('english')
+
+            word_frequencies = {}
+            for word in nltk.word_tokenize(formatted_article_text):
+                if word not in stopwords:
+                    if word not in word_frequencies.keys():
+                        word_frequencies[word] = 1
+                    else:
+                        word_frequencies[word] += 1
+                maximum_frequncy = max(word_frequencies.values())
+            for word in word_frequencies.keys():
+                word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
+                sentence_scores = {}
+            for sent in sentence_list:
+                for word in nltk.word_tokenize(sent.lower()):
+                    if word in word_frequencies.keys():
+                        if len(sent.split(' ')) < 30:
+                            if sent not in sentence_scores.keys():
+                                sentence_scores[sent] = word_frequencies[word]
+                            else:
+                                sentence_scores[sent] += word_frequencies[word]
+            
+            summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
+
+            summary = ' '.join(summary_sentences)
             print(summary)
+
 
             tempContent = summary
 
